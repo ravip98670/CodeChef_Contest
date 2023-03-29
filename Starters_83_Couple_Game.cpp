cbf14bf8cf965dbaad363d0911d81ccb4d438522
ravip98670 <ravispandey2002@gmail.com>
2023-03-29 23:22:52 +0530

    logical_question

---
+++ Starters_83_Couple_Game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test;
+	cin>>test;
+	while(test--){
+	    int bb,gg;
+	    cin>>bb>>gg;
+	    cout<<gg-bb<<endl;
+	}
+	return 0;
+}
