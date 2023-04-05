cf2c048083b783505b60212717d4e1d1a30d9ea6
ravip98670 <ravispandey2002@gmail.com>
2023-04-05 23:25:24 +0530

    Logic_based_question

---
+++ Starters_84_Chef_and_Donation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test;
+	cin>>test;
+	while(test--){
+	    int f,s;
+	    cin>>f>>s;
+	    cout<<s-f<<endl;
+	}
+	return 0;
+}
