715f03c8520ba81b6c5fe9d888e320ed6e98c4fd
ravip98670 <ravispandey2002@gmail.com>
2023-04-27 00:00:16 +0530

    easy_problem

---
+++ Starters_87_Four_Tickets.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test;
+	cin>>test;
+	while(test--){
+	    int tt;
+	    cin>>tt;
+	    if(4*tt <= 1000)
+	        cout<<"Yes"<<endl;
+	   else
+	   cout<<"No"<<endl;
+	}
+	return 0;
+}
