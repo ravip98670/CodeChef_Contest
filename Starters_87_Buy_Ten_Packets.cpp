cc54c096acba3aad6b16ee42d19f5f1293a8a0a1
ravip98670 <ravispandey2002@gmail.com>
2023-04-27 00:01:21 +0530

    logical_concept

---
+++ Starters_87_Buy_Ten_Packets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test;
+	cin>>test;
+	while(test--){
+	    int f,s;
+	    cin>>f>>s;
+	    
+	    cout<<(2*s) + f<<endl; 
+	}
+	return 0;
+}
